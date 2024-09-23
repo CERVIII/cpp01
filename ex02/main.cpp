f7ca2ff2ea12e7ca5d82c7d5ceb684f255255347
Pablo Cervilla miguel <pcervill@c3r3s3.42madrid.com>
2024-09-23 17:59:46 +0200

    ex00 & ex01 & ex02 completed

---
+++ ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcervill <pcervill@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 17:02:18 by pcervill          #+#    #+#             */
+/*   Updated: 2024/09/23 17:46:46 by pcervill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main()
+{
+	std::string str;
+	std::string *stringPTR;
+	std::string &stringREF = str;
+
+	str = "HI THIS IS BRAIN";
+	stringPTR = &str;
+
+	std::cout << "Address of the str: " << &str << std::endl;
+	std::cout << "Address of the stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address of the stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of the str: " << str << std::endl;
+	std::cout << "Value of the stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of the stringREF: " << stringREF << std::endl;
+	return (0);
+}
